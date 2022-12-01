82bb35343a05da523815b6ec86f6012d7c4709bf
Violeta Doncheva <violeta.doncheva90@gmail.com>
2022-12-01 10:47:02 +0100

    OneToMany_mappedBy

--- src/DataFixtures/ProgramFixtures.php
+++ src/DataFixtures/ProgramFixtures.php
@@ -3,59 +3,36 @@
 namespace App\DataFixtures;
 
 use App\Entity\Program;
+use App\DataFixtures\CategoryFixtures;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
 class ProgramFixtures extends Fixture implements DependentFixtureInterface
 {
-    public function load(ObjectManager $manager)
-
+    public const PROGRAM_LOOP = 5;
+    
+    public function load(ObjectManager $manager): void
     {
-
+        foreach(CategoryFixtures::CATEGORIES as $category){
+          for($i = 0 ; $i < 5 ; $i++){
         $program = new Program();
-
-        $program->setTitle('Walking dead');
-
-        $program->setSynopsis('Des zombies envahissent la terre');
-
-        $program->setCategory($this->getReference('category_Action'));
-
+        $program->setTitle('Titre du film ' . $i);
+        $program->setSynopsis('This is test');
+        $program->setCategory($this->getReference('category_' . $category));
+        $this->addReference('program_', $program);
         $manager->persist($program);
-
+          }
+        }
         $manager->flush();
-
     }
 
-
     public function getDependencies()
-
     {
 
         return [
-
           CategoryFixtures::class,
-
         ];
 
     }
-    // public function load(ObjectManager $manager): void
-    // {
-    // //     foreach(CategoryFixtures::CATEGORIES as $category){
-    // //         for($i = 0 ; $i < 5 ; $i++){
-    // //             $program = new Program();
-    // //             $program->setTitle('Titre du film ' . $i);
-    // //             $program->setSynopsis('This is test');
-    // //             $program->setCategory($this->getReference('category_' . $category));
-    // //             $manager->persist($program);
-    // //         }
-    // //     }
-    // //     $manager->flush();
-    // // }
-    // // public function getDependencies()
-    // // {
-    // //     return [
-    // //         CategoryFixtures::class,
-    // //     ];
-    // // }
 }
