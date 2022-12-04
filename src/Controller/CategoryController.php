b3d9eaae9727cc697cec54c907e714ad03077a85
Violeta Doncheva <violeta.doncheva90@gmail.com>
2022-12-04 21:54:45 +0100

    Form for adding on new category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,12 +2,14 @@
 // src/Controller/ProgramController.php
 namespace App\Controller;
 
-
+use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Repository\ProgramRepository;
 use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class CategoryController extends AbstractController
 {
@@ -19,6 +21,44 @@ class CategoryController extends AbstractController
             'categories' => $categories,
         ]);
     }
+    
+    #[Route('/new', name: 'new')]
+    public function new(Request $request, CategoryRepository $categoryRepository): Response
+    {
+        $category = new Category();
+       
+        // Create the form, linked with $category
+        $form = $this->createForm(CategoryType::class, $category);
+        
+        // Get data from HTTP request
+
+    $form->handleRequest($request);
+
+    // Was the form submitted ?
+    if ($form->isSubmitted()) {
+        $categoryRepository->save($category, true);            
+
+
+        // Redirect to categories list
+
+        return $this->redirectToRoute('category_index');
+
+        // Deal with the submitted data
+        // For example : persiste & flush the entity
+        // And redirect to a route that display the result
+
+    }
+        // Render the form (best practice)
+        return $this->renderForm('category/new.html.twig', [
+            'form' => $form,
+        ]);
+
+        // Alternative
+        // return $this->render('category/new.html.twig', [
+        //   'form' => $form->createView(),
+        // ]);
+    }
+
     #[Route('/category/{categoryName}/', requirements: ['category'=>'{categoryName}'], name: 'category_show')]
     public function showCategory(string $categoryName, ProgramRepository $programRepository, CategoryRepository $categoryRepository): Response
     {
