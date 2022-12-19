6c7d27cf8f891992a8899f1885a59cc61abcc587
Violeta Doncheva <violeta.doncheva90@gmail.com>
2022-12-19 11:50:20 +0100

    quest_18

--- src/DataFixtures/ActorFixtures.php
+++ src/DataFixtures/ActorFixtures.php
@@ -4,15 +4,14 @@ namespace App\DataFixtures;
 
 use Faker\Factory;
 use App\Entity\Actor;
-use App\DataFixtures\ProgramFixtures;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
-
 class ActorFixtures extends Fixture implements DependentFixtureInterface
 {
     public const ACTORS = 10;
+    public const SERIES_BY_ACTOR = 3;
 
     public function load(ObjectManager $manager): void
     {
@@ -21,12 +20,14 @@ class ActorFixtures extends Fixture implements DependentFixtureInterface
         foreach (CategoryFixtures::CATEGORIES as $category => $categoryName) {
             for ($i = 0; $i < ProgramFixtures::PROGRAM_NUMBER; $i++) {
                 for ($iActor = 1; $iActor < self::ACTORS; $iActor++) {
-                        $name = $faker->lastName() . ' ' . $faker->firstName();
-                        $actor = new Actor();
-                        $actor->setName($name);
-                        $program = $this->getReference( 'category_' . $categoryName . '_program_' . $i);
+                    $name = $faker->lastName() . ' ' . $faker->firstName();
+                    $actor = new Actor();
+                    $actor->setName($name);
+                    for ($j = 1; $j < self::SERIES_BY_ACTOR; $j++) {
+                        $program = $this->getReference('category_' . $categoryName . '_program_' . $i);
                         $actor->addProgram($program);
                         $manager->persist($actor);
+                    }
                 }
                 $manager->flush();
             }
