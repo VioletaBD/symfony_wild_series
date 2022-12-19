775189e247820d13a1304cf16e6df23890cc9a5e
Violeta Doncheva <violeta.doncheva90@gmail.com>
2022-12-19 11:36:04 +0100

    quest_18

--- src/DataFixtures/ActorFixtures.php
+++ src/DataFixtures/ActorFixtures.php
@@ -2,27 +2,41 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
 use App\Entity\Actor;
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\DataFixtures\ProgramFixtures;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
-class ActorFixtures extends Fixture
+
+class ActorFixtures extends Fixture implements DependentFixtureInterface
 {
-    const ACTORS = [
-        'Emilia Clarke',
-        'Sophie Turner',
-        'Jason Momoa',
-        'Kit Harington',
-        'Peter Dinklage'
-    ];
+    public const ACTORS = 10;
 
     public function load(ObjectManager $manager): void
     {
-        foreach(self::ACTORS as $keyActor => $actorName) {
-        $actor = new Actor();
-        $actor->setName('$actorName');
-        $manager->persist($actor);
+        $faker = Factory::create();
+
+        foreach (CategoryFixtures::CATEGORIES as $category => $categoryName) {
+            for ($i = 0; $i < ProgramFixtures::PROGRAM_NUMBER; $i++) {
+                for ($iActor = 1; $iActor < self::ACTORS; $iActor++) {
+                        $name = $faker->lastName() . ' ' . $faker->firstName();
+                        $actor = new Actor();
+                        $actor->setName($name);
+                        $program = $this->getReference( 'category_' . $categoryName . '_program_' . $i);
+                        $actor->addProgram($program);
+                        $manager->persist($actor);
+                }
+                $manager->flush();
+            }
         }
-        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            ProgramFixtures::class
+        ];
     }
 }
